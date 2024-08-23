5329c573d1b71c8d041d61c1f2583c4172eea0ce
nitish1899 <nkword1899@gmail.com>
2024-08-23 11:25:38 +0530

    add bikes

---
+++ src/utils/bike.js
@@ -0,0 +1,122 @@
+export const bikes = [
+    {
+        "bike": "Hero HF Deluxe",
+        "cc": "100"
+    }, {
+        "bike": "Hero HF 100",
+        "cc": "100"
+    }, {
+        "bike": "Hero Splendor Plus",
+        "cc": "100"
+    }, {
+        "bike": "TVS Sport",
+        "cc": "100"
+    }, {
+        "bike": "Hero Splendor Plus Xtec",
+        "cc": "100"
+    }, {
+        "bike": "Hero splender",
+        "cc": "100"
+    }, {
+        "bike": "STAR CITY+",
+        "cc": "100"
+    }, {
+        "bike": "SHINE",
+        "cc": "100"
+    }, {
+        "bike": "TVS RAIDER",
+        "cc": "100"
+    }, {
+        "bike": "TVS Radeon",
+        "cc": "100"
+    }, {
+        "bike": "SPLENDER",
+        "cc": "100"
+    }, {
+        "bike": "Honda SP 125",
+        "cc": "100"
+    }, {
+        "bike": "Honda Shine 125",
+        "cc": "100"
+    }, {
+        "bike": "Honda CD110 Dream Deluxe",
+        "cc": "100"
+    }, {
+        "bike": "Hero Super Splender XTECT",
+        "cc": "125"
+    }, {
+        "bike": "Honda Shine (DISC)",
+        "cc": "125"
+    }, {
+        "bike": "Pulsar NS 125",
+        "cc": "125"
+    }, {
+        "bike": "Hero Super Splender XTECT",
+        "cc": "125"
+    }, {
+        "bike": "Honda Shine 125 (DISC)",
+        "cc": "125"
+    }, {
+        "bike": "Hero Xtreme 125R",
+        "cc": "125"
+    }, {
+        "bike": "Glamour",
+        "cc": "125"
+    }, {
+        "bike": "PASSION +",
+        "cc": "125"
+    }, {
+        "bike": "GLAMOUR XTECK",
+        "cc": "125"
+    }, {
+        "bike": "PASSION XTECK",
+        "cc": "125"
+    }, {
+        "bike": "PULSAR 125",
+        "cc": "125"
+    }, {
+        "bike": "HERO XTREME 160R",
+        "cc": "150"
+    }, {
+        "bike": "Honda SP160",
+        "cc": "150"
+    }, {
+        "bike": "YAMAHA FZX",
+        "cc": "150"
+    }, {
+        "bike": "SUZUKI GIXERSF",
+        "cc": "150"
+    }, {
+        "bike": "Honda UNICORN",
+        "cc": "150"
+    }, {
+        "bike": "Bajaj Pulsar 150",
+        "cc": "150"
+    }, {
+        "bike": "Avenger",
+        "cc": "150"
+    }, {
+        "bike": "Honda HORNET",
+        "cc": "150"
+    }, {
+        "bike": "HERO XPULSE",
+        "cc": "200"
+    }, {
+        "bike": "TVS Apache",
+        "cc": "150"
+    }, {
+        "bike": "Yamaha FZ S FI",
+        "cc": "150"
+    }, {
+        "bike": "YAMAHA MT15",
+        "cc": "150"
+    }, {
+        "bike": "YAMAHA R15M",
+        "cc": "150"
+    }, {
+        "bike": "Royel Enfield",
+        "cc": "300"
+    }, {
+        "bike": "KTM RC 200",
+        "cc": "150"
+    }]
