6a337604cd138ae7357b7c03842cccae87c63c20
nitish1899 <nkword1899@gmail.com>
2024-08-27 17:48:55 +0530

    add schema

--- src/models/bookingKit.model.js
+++ src/models/bookingKit.model.js
@@ -2,42 +2,6 @@ import mongoose from "mongoose";
 
 // Define the BookingKit schema
 const BookingKitSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    aadhar: {
-        type: String,
-        required: true,
-    },
-    dlno: {
-        type: String,
-        required: true,
-    },
-    dob: {
-        type: String,
-        required: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    amount: {
-        type: String,
-        required: true,
-    },
     razorpay_order_id: {
         type: String,
         required: true,
@@ -50,8 +14,14 @@ const BookingKitSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    visitorBikeDetails: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Visitorbikedetails",
+        required: true, 
+    }
 });
 
+// Create and export the BookingKit model
 const BookingKit = mongoose.model("BookingKit", BookingKitSchema);
 
 export { BookingKit };
