184f91158046a061c1208f7e660aaf6e93e99db1
nitish1899 <nkword1899@gmail.com>
2024-09-23 16:49:12 +0530

    refined message

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.get("/api/getkey", (req, res) =>
     res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
 
-app.use('/', (req, res) => res.json({ message: 'Welcome to CO2e Calculator' }));
+app.use('/', (req, res) => res.json({ message: 'Welcome to the world of KGV Hybrid E-mobility' }));
 
 app.use((err, req, res, next) => {
     console.log(err.stack); // Log the error (optional)
